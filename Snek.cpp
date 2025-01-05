c3e3d18059f2db58832d1a4626b212aeff20c795
Ankush Hegde <ankushhegde30@gmail.com>
2025-01-05 19:15:54 +0530

    Snake game code structure

---
+++ Snek.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<conio.h>
+#include<windows.h>
+
+using namespace std;
+
+bool gameOver;
+const int height = 20;
+const int width = 20;
+
+void Setup(){
+    gameOver = false;
+}
+
+void Draw(){}
+
+void Input(){}
+
+void Logic(){}
+
+int main(){
+
+    return 0;
+}
